0c57c7d6cee0a1f4e42058a6fde17cef1a6714db
thomas berhane <berhane097@gmail.com>
2019-07-23 12:51:36 +0200

    validated the comment , playlist and playlist_videos - models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :youtube_account
+  belongs_to :videos, dependent: :destroy
+
+  validates :text, presence: true
 end
--- app/models/playlist_video.rb
+++ app/models/playlist_video.rb
@@ -1,2 +1,4 @@
 class PlaylistVideo < ApplicationRecord
+  belongs_to :videos
+  belongs_to :playlists
 end
