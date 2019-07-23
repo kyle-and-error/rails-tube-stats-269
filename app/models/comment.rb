024cbc40ea61a62e9ec473c3ce23ce437fae55a7
Kyle Cho <kylescho99@gmail.com>
2019-07-23 13:49:10 +0200

    Finished validations and associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :youtube_account
+  belongs_to :watcher, class_name: 'YoutubeAccount'
   belongs_to :videos, dependent: :destroy
 
   validates :text, presence: true
