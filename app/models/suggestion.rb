cd2e521fb9aaa5a4d63b2332cdd7b175ec6c6c7f
Kyle Cho <kylescho99@gmail.com>
2019-07-30 15:51:28 +0200

    finished video suggestions

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -40,16 +40,36 @@ class Suggestion < ApplicationRecord
       message = "We reccomend that you unsubscribe from #{watch.creator}. "
       Suggestion.create!(watcher: watcher, type: "Channel", action: "Unsubscribe", creator: watch.creator, message: message)
     end
-    Suggestion.create! << Watch.least_watched_by(watcher)
+    # Suggestion.create! << Watch.least_watched_by(watcher)
   end
 
   def self.create_playlist_suggestions
-
     # TODO
+
   end
 
-  def self.create_video_suggestions
+  def self.create_video_suggestions(watcher)
     # TODO
+    watched_videos = WatchedVideo.where(watch: Watch.where(watcher: watcher)).to_a
+    array_video = array.map {|wv| wv.video}
+    array_video.unique!
+    videos_count = Hash.new(0)
+    array_video.each do |v|
+      watched_videos.each do |wv|
+        if wv.video == v
+          suggested_videos[v] += 1
+        end
+      end
+    end
+    videos_count.each do |video, count|
+      if count > 4
+        message = ""
+        Suggestion.create!(watcher: watcher, type: "Video", action: "Add to playlist", video: video, message: message)
+      elsif count > 2
+        messagee = ""
+        Suggestion.create!(watcher: watcher, type: "Video", action: "Like", video: video, message: message)
+      end
+    end
   end
 
   private
