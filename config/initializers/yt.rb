707cf3842a83b913bf6b2d87778c0926fd27b82b
Kyle Cho <kylescho99@gmail.com>
2019-08-01 21:55:48 +0200

    Changed client secrets

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -1,5 +1,5 @@
 Yt.configure do |config|
-  config.client_id = '576416227603-493bbagrdov99e836dfsqcmkcuftlm72.apps.googleusercontent.com'
-  config.client_secret = 'HNpJT6cp7r9S3PHN8hZMJ8aO'
+  config.client_id = '160634848499-2098m7kef2jp23fhncargel23hlupfr3.apps.googleusercontent.com'
+  config.client_secret = '-bN8Q4e_aU8M-nz_Sw8D3FVW'
   # config.api_key = 'AIzaSyDK-gujM6WpVAu8mafFZwYgxcfEWHswEkY'
 end
