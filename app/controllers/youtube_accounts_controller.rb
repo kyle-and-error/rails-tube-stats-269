ec7da04392825554932042d0610005fd435ec155
Kyle Cho <kylescho99@gmail.com>
2019-08-01 10:28:25 +0200

    QUick domain fix

--- app/controllers/youtube_accounts_controller.rb
+++ app/controllers/youtube_accounts_controller.rb
@@ -10,7 +10,7 @@ class YoutubeAccountsController < ApplicationController
     auth_client = client_secrets.to_authorization
     auth_client.update!(
       scope: ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/youtube.readonly', 'https://www.googleapis.com/auth/userinfo.email'],
-      redirect_uri: "http://#{DOMAIN}/youtube_accounts/new",
+      redirect_uri: "https://#{DOMAIN}/youtube_accounts/new",
       additional_parameters: {
         "access_type" => "offline",         # offline access
         "include_granted_scopes" => "true"  # incremental auth
